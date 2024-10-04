0098f06b0b91b519c6e05459ccd4be5c5e7d9e90
farhadshakiba1988 <farhadshakiba1988@gmail.com>
2024-10-04 22:14:57 +0330

    second commit create database and create form class and change view class

---
+++ web/form/__init__.py
@@ -0,0 +1,1 @@
+from .ReceiptForm import ProductForm
--- web/views/receipt_view.py
+++ web/views/receipt_view.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+from web.form.ReceiptForm import ReceiptForm
 
 
 class Receipt(View):
     def get(self, request):
-        return render(request, 'receipt.html')
+        form = ReceiptForm
+        return render(request, 'receipt.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ReceiptForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('conf:farhad')  # به صفحه‌ای هدایت می‌شوید پس از موفقیت
+        return render(request, 'receipt.html', {'form': form})
